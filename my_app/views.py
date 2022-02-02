c40902d27115e8b7eb3a42c1451c73056194e782
martin-dsp <kcs19542001@hotmail.com>
2022-02-02 13:11:12 +0900

    craigslist clone with ReactJS and Django

--- my_app/views.py
+++ my_app/views.py
@@ -9,15 +9,18 @@ from . import models
 BASE_CRAIGSLIST_URL = "https://losangeles.craigslist.org/search/?query={}"
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'base.html')
+
 
 def new_search(request):
     # POST = http protocol / get here is a method in Python.
     search = request.POST.get('search')
     models.Search.objects.create(search=search)
     final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
-    response = requests.get('https://losangeles.craigslist.org/search/sss?query=cat+tower')
+    response = requests.get(final_url)
     data = response.text
     soup = BeautifulSoup(data, features='html.parser')
 
@@ -28,16 +31,15 @@ def new_search(request):
     for post in post_listings:
         post_title = post.find('a', {'class': 'result-title hdrlnk'}).text
         post_url = post.find('a').get('href')
-        
+
         if post.find(class_='result-price'):
             post_price = post.find('span', {'class': 'result-price'}).text
         else:
-            post_price = 'N/A'    
+            post_price = 'N/A'
         final_postings.append((post_title, post_url, post_price))
-            
 
     stuff_for_frontend = {
         'search': search,
-        'final_postings': final_postings, 
+        'final_postings': final_postings,
     }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
